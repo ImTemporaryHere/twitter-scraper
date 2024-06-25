9f75f84e7d199d5a8483dfc0d48c782ebf483d36
ImTemporaryHere <soapaka@live.com>
2024-06-25 21:29:45 +0300

    feat: write cookies in file (for test-utils usage)

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -19,8 +19,9 @@ export async function getScraper(
   const password = process.env['TWITTER_PASSWORD'];
   const email = process.env['TWITTER_EMAIL'];
   const twoFactorSecret = process.env['TWITTER_2FA_SECRET'];
-  const cookies = process.env['TWITTER_COOKIES'];
   const proxyUrl = process.env['PROXY_URL'];
+  // const cookies = process.env['TWITTER_COOKIES'];
+  const cookies = await getCookiesIfExist()
   let agent: any;
 
   if (options.authMethod === 'cookies' && !cookies) {
@@ -51,13 +52,35 @@ export async function getScraper(
         return [input, init];
       },
     },
+    userAgent: process.env.USER_AGENT,
   });
 
   if (options.authMethod === 'password') {
     await scraper.login(username!, password!, email, twoFactorSecret);
+    const cookies = await scraper.getCookies();
+    await fs.writeFile('cookies.json', JSON.stringify(cookies), {
+      encoding: 'utf8',
+    });
   } else if (options.authMethod === 'cookies') {
-    await scraper.setCookies(JSON.parse(cookies!));
+    // await scraper.setCookies(JSON.parse(cookies!));
+    await scraper.setCookies(cookies);
   }
 
   return scraper;
+}
+
+export async function getCookiesIfExist() {
+  try {
+    const cookiesFilePath = 'cookies.json'
+    const cookiesString = await fs.readFile(cookiesFilePath, { encoding: 'utf8' });
+    const cookies = JSON.parse(
+      cookiesString
+    ).map((i: any) => new tough.Cookie(i));
+    return cookies
+  }
+  catch (e) {
+    console.warn(e)
+    return undefined
+  }
+
 }
